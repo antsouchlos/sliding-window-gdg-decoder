47f6149ed7a944b1082724978d79c0e50060ad97
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2026-02-16 16:41:18 +0100

    Set up proper project structure

--- setup.py
+++ setup.py
@@ -2,63 +2,61 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
-VERSION = "0.0.0"
-
-# Write version file
-with open("src/VERSION", "w") as f:
-    f.write(VERSION)
 
 # Define all extensions
 extensions = [
     Extension(
-        name="src.mod2sparse",
-        sources=["src/include/mod2sparse.c", "src/mod2sparse.pyx"],
-        include_dirs=[numpy.get_include(), "src/include"],
+        name="sw_gdg.c_util",
+        sources=["sw_gdg/c_util.pyx", "sw_gdg/cpp/mod2sparse.c"],
+        include_dirs=[numpy.get_include()],
         extra_compile_args=["-std=c11"],
     ),
     Extension(
-        name="src.c_util",
-        sources=["src/c_util.pyx", "src/include/mod2sparse.c"],
-        include_dirs=[numpy.get_include(), "src/include"],
+        name="sw_gdg.mod2sparse",
+        sources=["sw_gdg/cpp/mod2sparse.c", "sw_gdg/mod2sparse.pyx"],
+        include_dirs=[numpy.get_include(), "sw_gdg/cpp"],
         extra_compile_args=["-std=c11"],
     ),
     Extension(
-        name="src.bp_guessing_decoder",
+        name="sw_gdg.bp_guessing_decoder",
         sources=[
-            "src/bp_guessing_decoder.pyx",
-            "src/include/mod2sparse.c",
-            "src/include/bpgd.cpp",
+            "sw_gdg/bp_guessing_decoder.pyx",
+            "sw_gdg/cpp/mod2sparse.c",
+            "sw_gdg/cpp/bpgd.cpp",
         ],
         language="c++",
-        include_dirs=[numpy.get_include(), "src/include"],
+        include_dirs=[numpy.get_include(), "sw_gdg/cpp"],
         extra_compile_args=["-std=c11"],
     ),
     Extension(
-        name="src.osd_window",
+        name="sw_gdg.osd_window",
         sources=[
-            "src/osd_window.pyx",
-            "src/include/mod2sparse.c",
-            "src/include/mod2sparse_extra.cpp",
-            "src/include/bpgd.cpp",
+            "sw_gdg/osd_window.pyx",
+            "sw_gdg/cpp/mod2sparse.c",
+            "sw_gdg/cpp/mod2sparse_extra.cpp",
+            "sw_gdg/cpp/bpgd.cpp",
         ],
         language="c++",
-        include_dirs=[numpy.get_include(), "src/include"],
+        include_dirs=[numpy.get_include(), "sw_gdg/cpp"],
         extra_compile_args=["-std=c11"],
     ),
     Extension(
-        name="src.bp4_osd",
+        name="sw_gdg.bp4_osd",
         sources=[
-            "src/bp4_osd.pyx",
-            "src/include/mod2sparse.c",
-            "src/include/mod2sparse_extra.cpp",
-            "src/include/bpgd.cpp",
+            "sw_gdg/bp4_osd.pyx",
+            "sw_gdg/cpp/mod2sparse.c",
+            "sw_gdg/cpp/mod2sparse_extra.cpp",
+            "sw_gdg/cpp/bpgd.cpp",
         ],
         language="c++",
-        include_dirs=[numpy.get_include(), "src/include"],
+        include_dirs=[numpy.get_include(), "sw_gdg/cpp"],
         extra_compile_args=["-std=c11"],
     ),
 ]
 
 setup(
-    ext_modules=cythonize(extensions),
+    name="sw_gdg",
+    ext_modules=cythonize(
+        extensions, compiler_directives={"language_level": 3, "profile": False}
+    ),
 )
